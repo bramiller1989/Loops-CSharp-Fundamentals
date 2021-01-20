4a64135b549c1858ebb7e960446ac7c157aa391b
bramiller1989 <bramiller1989@gmail.com>
2021-01-20 14:15:17 -0600

    add exercise 5

--- Program.cs
+++ Program.cs
@@ -65,6 +65,25 @@ namespace Loops
             }
         }
 
+        public void Exercise5()
+        {
+            Console.Write("Enter comma separated numbers:");
+            var input = Console.ReadLine();
+
+            var numbers = input.Split(',');
+
+            var max = Convert.ToInt32(numbers[0]);
+
+            foreach (var str in numbers)
+            {
+                var number = Convert.ToInt32(str);
+                if (number > max)
+                    max = number;
+            }
+
+            Console.WriteLine("Max is " + max);
+        }
+
         static void Main(string[] args)
         {
             
